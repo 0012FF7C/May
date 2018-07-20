1899d7bd91ca136b48c8597a69037204a22d60a7
CodingJiang <Coding_jiang@outlook.com>
2018-07-20 14:35:14 +0800

    大作业项目建好了

---
+++ Fourth/src/Node/Thread/Heart.java
@@ -0,0 +1,13 @@
+package Node.Thread;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+public class Heart extends Thread{
+	ArrayList Neibours;
+	public Heart(ArrayList Neibours) {
+		this.Neibours=Neibours;
+	}
+	public void run() {
+		
+	}
+}
