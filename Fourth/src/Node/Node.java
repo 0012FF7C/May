a4e5ef62aee5d97de9239c41bbf1e21f03f1a9ca
CodingJiang <Coding_jiang@outlook.com>
2018-07-21 12:03:46 +0800

    7.21 get getlen

--- Fourth/src/Node/Node.java
+++ Fourth/src/Node/Node.java
@@ -21,6 +21,7 @@ public class Node {
 	public static Heart  		heart;
 	
 	public static String ConfigFilePath;
+	public static String ResponseNodes;
 	
 	public static DatagramPacket CreateMessage(String kind, String Messege,int port) throws UnknownHostException{
 		String content = kind+" ";
@@ -64,6 +65,35 @@ public class Node {
 			}
 		}
 		return null;
-		
+	}
+
+	public static int getLen(String s) {
+		int num = 0;
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(i)==' ') {
+				num++;
+			}
+		}
+		return num+1;
+	}
+	public static String get(String s, int index ) {
+		int beg = 0;
+		int end = 0;
+		int num = -1;
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(i)==' ') {
+				num++;
+				beg=end;
+				end=i;
+				if(num==index) {
+					return s.substring(beg, end);
+				}
+			}
+		}
+		return null;
+	}
+	public static void main(String [] args) {
+		String s = new String("fjdksah 123 5798 75634");
+		System.out.println(getLen(s));
 	}
 }
